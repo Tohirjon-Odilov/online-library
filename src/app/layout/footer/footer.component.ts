34ba4984e4981419a60d29eee368273ea3d74ee6
tohirjon-odilov <shahanshoh819@gmail.com>
2024-11-26 17:49:22 +0500

    fixed some fiels

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+
+  constructor(private toastr: ToastrService) {}
   jumpTo(section: string) {
     const element = document.getElementById(section);
     if (element) {
       element.scrollIntoView({ behavior: 'smooth' });
     }
+  }
+
+  showReportInfo(): void {
+    this.toastr.info('Iltimos ctrl+enter tugmasidan foydalaning!', 'Info');
   }
 }
