c3808b94534875020749fc0f8e9884f6cc6722a9
Tohirjon-Odilov <shahanshoh819@gmail.com>
2025-01-31 10:30:07 +0500

    Refactor home component styles and improve author display with dynamic data integration

--- src/app/features/home/components/home/home.component.ts
+++ src/app/features/home/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import { LoggerService } from '../../../../core/services/logger.service';
 import { CategoryService } from '../../services/category.service';
@@ -6,26 +6,34 @@ import { BookService } from '../../services/book.service';
 import { List, shuffle } from 'lodash';
 import { API_URLS } from '../../../../config/constants';
 import { environment } from '../../../../../environments/environment';
+import {AuthorService} from "../../services/author.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  baseUrl = environment.baseUrl
+  randomBook: any
+  randomCategories: Array<any> = []
+  topBooks: Array<any> = []
+  authors: any;
+
+  carouselImages = [
+    'https://via.placeholder.com/800x300/FF5733/FFFFFF?text=Slide+1',
+    'https://via.placeholder.com/800x300/33FF57/FFFFFF?text=Slide+2',
+    'https://via.placeholder.com/800x300/5733FF/FFFFFF?text=Slide+3',
+  ];
 
   constructor(
     private router: Router,
     private logger: LoggerService,
     private categoryService: CategoryService,
     private bookService: BookService,
+    private authorService: AuthorService
   ){}
 
-  randomBook: any
-  randomCategories: Array<any> = []
-  topBooks: Array<any> = []
-  baseUrl = environment.baseUrl
-  
   ngOnInit(): void {
     this.bookService.getRandomBook().subscribe(res => {
       this.randomBook = res;
@@ -38,19 +46,22 @@ export class HomeComponent {
     this.categoryService.getCategories().subscribe(res => {
       this.randomCategories = res;
     })
+
+    this.getAllAuthor();
    }
 
    randomImage(imageArray: any): any {
     return shuffle(imageArray).slice(0, 4);
    }
-  
+
    randomCategoryImage(imageArray: Array<any>): any {
     return shuffle(imageArray).slice(0, 3);
    }
 
-   carouselImages = [
-    'https://via.placeholder.com/800x300/FF5733/FFFFFF?text=Slide+1',
-    'https://via.placeholder.com/800x300/33FF57/FFFFFF?text=Slide+2',
-    'https://via.placeholder.com/800x300/5733FF/FFFFFF?text=Slide+3',
-  ];
+  getAllAuthor(): void {
+    this.authorService.getAuthors().subscribe(authors => {
+      this.authors = authors
+      console.log(authors);
+    })
+  }
 }
